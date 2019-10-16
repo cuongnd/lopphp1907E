9d524124a2b6427cc60c204403d6f6783ac13d50
cuongnd <nguyendinhcuong@gmail.com>
2019-10-16 19:04:23 +0700

    update add product

--- ecommerce/paynow.php
+++ ecommerce/paynow.php
@@ -11,7 +11,7 @@
 
 <div class="container">
     <?php require_once FRONT_END_ROOT_PATH . "/components/banner_menu.php"; ?>
-
+    <?php $msg=""; ?>
     <?php if (isset($_GET['action']) && $_GET['action'] == "pay_now") {
         //save oder
         $total = 0;
@@ -32,10 +32,28 @@
                 VALUES (NULL, '$user_id', '$last_name', '$first_name', '$phone_number', '$address', '0');";
         mysqli_query($connection, $query);
         $new_customer_insert_id = mysqli_insert_id($connection);
+        $random_order_number = mt_rand(1, time());
+        $query="INSERT INTO `orders` 
+                (`id`, `order_number`, `customer_id`, `status`, `total`)
+        VALUES (NULL, 'ORDER_$random_order_number', '$new_customer_insert_id', 'pending', '$total');";
+        mysqli_query($connection, $query);
+        $new_order_insert_id = mysqli_insert_id($connection);
+        foreach ($_SESSION['cart'] as $key => $product) {
+            $product_id=$product['product_id'];
+            $current_price = $product['sale_price'] != 0 ? $product['sale_price'] : $product['price'];
+            $total_current_product = $current_price * $product['quality'];
+            $query="INSERT INTO `order_product` 
+                    (`order_id`, `product_id`, `price`) 
+            VALUES ('$new_order_insert_id', '$product_id', '$total_current_product');";
+            mysqli_query($connection, $query);
 
+        }
+        $msg="Cám ơn bạn đã mua hàng của chúng tôi";
+        $_SESSION['cart']=array();
 
     } ?>
-
+    <h3><?php echo $msg ?></h3>
+    <a href="<?php echo FRONT_END_ROOT_SITE ?>">Quay về trang chủ</a>
     <?php require_once FRONT_END_ROOT_PATH . "/components/footer.php"; ?>
 
 
